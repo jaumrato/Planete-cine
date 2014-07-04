d7e6eadec44a272aea219395641412bc8e743cfb
Dammmien <d.damien@live.fr>
2014-07-04 16:57:49 +0200

    init

--- www/js/index.js
+++ www/js/index.js
@@ -20,35 +20,33 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-},    // Bind Event Listeners
+    },
+    // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-
-        document.addEventListener('deviceready', function() {
-
-            navigator.notification.vibrate(time)
-
-        }, false);
-
+        document.addEventListener( 'deviceready', this.onDeviceReady, false );
+        document.addEventListener( 'deviceready', function() {
+            navigator.notification.vibrate( time )
+        }, false );
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.receivedEvent( 'deviceready' );
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function( id ) {
+        var parentElement = document.getElementById( id );
+        var listeningElement = parentElement.querySelector( '.listening' );
+        var receivedElement = parentElement.querySelector( '.received' );
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute( 'style', 'display:none;' );
+        receivedElement.setAttribute( 'style', 'display:block;' );
 
-        console.log('Received Event: ' + id);
+        console.log( 'Received Event: ' + id );
     }
 };
