d3a89452b84d1ffac5f4f1956317ee8dae1ff5c5
Damien Duhamel <d.damien@live.fr>
2014-07-05 16:17:55 +0200

    Network

--- js/index.js
+++ js/index.js
@@ -10,6 +10,23 @@ var app = {
 
     onDeviceReady: function() {
         navigator.notification.vibrate( 300 );
+        document.getElementById('main').innerHTML = "Test de remplacement";        
+        app.checkConnection();
+    },
+
+    checkConnection: function() {
+        var networkState = navigator.network.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Connexion inconnue';
+        states[Connection.ETHERNET] = 'Connexion Ethernet';
+        states[Connection.WIFI]     = 'Connexion WiFi';
+        states[Connection.CELL_2G]  = 'Connexion cellulaire 2G';
+        states[Connection.CELL_3G]  = 'Connexion cellulaire 3G';
+        states[Connection.CELL_4G]  = 'Connexion cellulaire 4G';
+        states[Connection.NONE]     = 'Pas de connexion réseau';
+
+        alert('Type de connexion : ' + states[networkState]);
     }
 
 };
