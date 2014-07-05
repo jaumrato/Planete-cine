984eb7a97c419dbea3083f2c5507cf7be4c21231
Damien Duhamel <d.damien@live.fr>
2014-07-05 16:08:01 +0200

    Network

--- js/index.js
+++ js/index.js
@@ -5,30 +5,11 @@ var app = {
     },
 
     bindEvents: function() {
-        document.addEventListener( 'deviceready', this.onDeviceReady.bind( this ), false );
+        document.addEventListener( 'deviceready', this.onDeviceReady, false );
     },
 
     onDeviceReady: function() {
         navigator.notification.vibrate( 300 );
-        this.checkConnection();
-    },
-
-    checkConnection: function(){
-
-        var networkState = navigator.connection.type,
-            states = {};
-
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.CELL]     = 'Cell generic connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        document.getElementById('main').innerHTML = 'Connection type: ' + states[networkState];
-        
     }
 
 };
