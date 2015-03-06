dce590679edf3fa94dc66a8db6ba2e2b4f8019f7
Dammmien <d.damien@live.fr>
2015-03-06 10:35:22 +0100

    geolocation bug

--- js/app.js
+++ js/app.js
@@ -1,38 +1,34 @@
-var app = angular.module( 'Application', [ 'ngRoute' ] );
+var app;
 
-app.config( function( $routeProvider, $locationProvider ) {
+function onLoad() {
+    document.addEventListener( "deviceready", onDeviceReady, false );
+}
 
-    $routeProvider.when( '/', {
-        templateUrl: 'templates/home.html',
-        controller: 'homeCtrl'
-    } ).when( '/theaters', {
-        templateUrl: 'templates/theaters.html',
-        controller: 'theatersCtrl'
-    } ).when( '/movies', {
-        templateUrl: 'templates/movies.html',
-        controller: 'moviesCtrl'
-    } ).when( '/showtimes/:theaterCode', {
-        templateUrl: 'templates/showtimes.html',
-        controller: 'showtimesCtrl'
-    } ).when( '/showtimesByTheater/:movieCode', {
-        templateUrl: 'templates/showtimesByTheater.html',
-        controller: 'showtimesByTheaterCtrl'
-    } ).otherwise( {
-        redirectTo: '/'
+function onDeviceReady() {
+
+    angular.bootstrap( document.body, [ 'Application' ] );
+
+    app = angular.module( 'Application', [ 'ngRoute' ] );
+
+    app.config( function( $routeProvider, $locationProvider ) {
+        $routeProvider.when( '/', {
+            templateUrl: 'templates/home.html',
+            controller: 'homeCtrl'
+        } ).when( '/theaters', {
+            templateUrl: 'templates/theaters.html',
+            controller: 'theatersCtrl'
+        } ).when( '/movies', {
+            templateUrl: 'templates/movies.html',
+            controller: 'moviesCtrl'
+        } ).when( '/showtimes/:theaterCode', {
+            templateUrl: 'templates/showtimes.html',
+            controller: 'showtimesCtrl'
+        } ).when( '/showtimesByTheater/:movieCode', {
+            templateUrl: 'templates/showtimesByTheater.html',
+            controller: 'showtimesByTheaterCtrl'
+        } ).otherwise( {
+            redirectTo: '/'
+        } );
     } );
 
-    // $locationProvider.html5Mode( {
-    //     enabled: true,
-    //     requireBase: false
-    // } );
-    // $locationProvider.hashPrefix( '!' );
-
-} );
-
-document.addEventListener( 'deviceready', function() {
-    angular.bootstrap( document.body, [ 'Application' ] );
-}, false );
-
-// document.addEventListener( "DOMContentLoaded", function( event ) {
-//     angular.bootstrap( document.body, [ 'Application' ] );
-// }, false );
+}
