60c732b318ba136816b8c10eaf606c87fbec6962
Dammmien <d.damien@live.fr>
2015-03-06 10:55:04 +0100

    geolocation bug

--- js/app.js
+++ js/app.js
@@ -1,34 +1,29 @@
-var app;
+var app = angular.module( 'Application', [ 'ngRoute' ] );
 
-function onLoad() {
-    document.addEventListener( "deviceready", onDeviceReady, false );
-}
+app.config( function( $routeProvider, $locationProvider ) {
 
-function onDeviceReady() {
-
-    angular.bootstrap( document.body, [ 'Application' ] );
-
-    app = angular.module( 'Application', [ 'ngRoute' ] );
-
-    app.config( function( $routeProvider, $locationProvider ) {
-        $routeProvider.when( '/', {
-            templateUrl: 'templates/home.html',
-            controller: 'homeCtrl'
-        } ).when( '/theaters', {
-            templateUrl: 'templates/theaters.html',
-            controller: 'theatersCtrl'
-        } ).when( '/movies', {
-            templateUrl: 'templates/movies.html',
-            controller: 'moviesCtrl'
-        } ).when( '/showtimes/:theaterCode', {
-            templateUrl: 'templates/showtimes.html',
-            controller: 'showtimesCtrl'
-        } ).when( '/showtimesByTheater/:movieCode', {
-            templateUrl: 'templates/showtimesByTheater.html',
-            controller: 'showtimesByTheaterCtrl'
-        } ).otherwise( {
-            redirectTo: '/'
-        } );
+    $routeProvider.when( '/', {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+    } ).when( '/theaters', {
+        templateUrl: 'templates/theaters.html',
+        controller: 'theatersCtrl'
+    } ).when( '/movies', {
+        templateUrl: 'templates/movies.html',
+        controller: 'moviesCtrl'
+    } ).when( '/showtimes/:theaterCode', {
+        templateUrl: 'templates/showtimes.html',
+        controller: 'showtimesCtrl'
+    } ).when( '/showtimesByTheater/:movieCode', {
+        templateUrl: 'templates/showtimesByTheater.html',
+        controller: 'showtimesByTheaterCtrl'
+    } ).otherwise( {
+        redirectTo: '/'
     } );
 
-}
+} );
+
+document.addEventListener( 'deviceready', function() {
+    alert( 'ok' );
+    angular.bootstrap( document.body, [ 'Application' ] );
+}, false );
