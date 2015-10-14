ad8b07d5f03b24a0b8fa40bf0df5d1f55ab5f6f3
Dammmien <d.damien@live.fr>
2015-10-14 11:55:43 +0200

    test navigator.onLine

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,4 +1,4 @@
-app.controller( 'mainCtrl', function( $scope, Model, Service ) {
+app.controller( 'mainCtrl', function( $scope, Model, Service, $window ) {
 
     $scope.model = Model;
 
@@ -6,24 +6,35 @@ app.controller( 'mainCtrl', function( $scope, Model, Service ) {
         Model.previousLocation = previous;
     } );
 
-    $scope.$watch( function() {
-        return navigator.connection;
-    }, function( newVal, oldVal ) {
-        var networkState = navigator.connection.type;
+    // $scope.$watch( function() {
+    //     return navigator.connection;
+    // }, function( newVal, oldVal ) {
+    //     var states = {};
+    //     states[ Connection.UNKNOWN ] = false;
+    //     states[ Connection.ETHERNET ] = true;
+    //     states[ Connection.WIFI ] = true;
+    //     states[ Connection.CELL_2G ] = true;
+    //     states[ Connection.CELL_3G ] = true;
+    //     states[ Connection.CELL_4G ] = true;
+    //     states[ Connection.CELL ] = true;
+    //     states[ Connection.NONE ] = false;
+    //     Model.online = states[ navigator.connection.type ];
+    // } );
+    //
 
-        var states = {};
-        states[ Connection.UNKNOWN ] = false;
-        states[ Connection.ETHERNET ] = true;
-        states[ Connection.WIFI ] = true;
-        states[ Connection.CELL_2G ] = true;
-        states[ Connection.CELL_3G ] = true;
-        states[ Connection.CELL_4G ] = true;
-        states[ Connection.CELL ] = true;
-        states[ Connection.NONE ] = false;
+    $scope.online = navigator.onLine;
 
-        Model.connection = newVal;
-        Model.connection.foo = Connection;
-    } );
+    $window.addEventListener( "offline", function() {
+        $scope.$apply( function() {
+            $scope.online = false;
+        } );
+    }, false );
+
+    $window.addEventListener( "online", function() {
+        $scope.$apply( function() {
+            $scope.online = true;
+        } );
+    }, false );
 
     Service.getApiKey();
 
