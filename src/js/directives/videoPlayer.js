395c74eb64958c168cdafe4cc70362c39cdaefcf
Dammmien <d.damien@live.fr>
2015-10-16 17:04:33 +0200

    Stop video when not visible

--- src/js/directives/videoPlayer.js
+++ src/js/directives/videoPlayer.js
@@ -6,13 +6,24 @@ app.directive( 'videoPlayer', function( Model ) {
         link: function( $scope, $elements, $controller, $attr ) {
             var video = $elements[ 0 ],
                 source = document.createElement( 'source' );
+
             source.src = Model.movieDetails.trailer.url;
             source.type = 'video/mp4';
+
             video.appendChild( source );
+
             if ( Model.movieDetails.trailer.thumbnail ) {
                 video.poster = Model.movieDetails.trailer.thumbnail.replace( '/videothumbnails', '/r_720_x/videothumbnails' );
             }
-            video.style.display = 'inline';
+
+            // Stop video when app is in background
+            document.addEventListener( 'pause', video.pause.bind( video ), false );
+            window.addEventListener( 'blur', video.pause.bind( video ), false );
+
+            // Stop video when user scroll and video isn't visible
+            video.parentElement.addEventListener( 'scroll', function() {
+                if ( this.scrollTop > video.offsetHeight ) video.pause();
+            }, false );
         }
     };
 } );
